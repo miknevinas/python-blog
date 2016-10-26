2e1d580e21113739e5700717b454a6ec1dab7fdd
Adam McNevin <miknevinas@gmail.com>
2016-10-26 09:45:55 -0600

    Fix

--- blog/views.py
+++ blog/views.py
@@ -21,9 +21,9 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -35,6 +35,6 @@ def post_edit(request, pk):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm(instance=post)
-            return render(request, 'blog/post_edit.html', {'form': form})
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
